837deffd06fd07aaea386752a625bedbc914fe20
sumon232 <sumonmia4526@gmail.com>
2025-01-07 04:05:18 +0600

    Alhamdulillah banner is done

--- src/Pages/Shared/NavBer/NavBer.jsx
+++ src/Pages/Shared/NavBer/NavBer.jsx
@@ -25,7 +25,7 @@ const NavBer = () => {
   );
 
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar fixed  z-10 max-w-screen-xl mx-auto bg-opacity-20 text-white bg-black">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
